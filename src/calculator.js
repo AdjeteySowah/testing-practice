be3a04ec24ac045ae5f8cccbbe2b88e167268e99
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-11-19 13:23:48 +0000

    feat(calculator): add multiply() and divide() methods

--- src/calculator.js
+++ src/calculator.js
@@ -18,4 +18,21 @@ export const calculator = {
     }
     return num1 - num2;
   },
+
+  multiply(num1, num2) {
+    if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
+      throw new Error(`Expects all arguments of multiply() to be finite numbers`);
+    }
+    return num1 * num2;
+  },
+
+  divide(num1, num2) {
+    if (num2 === 0) {
+      throw new Error(`Cannot divide by zero`);
+    }
+    if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
+      throw new Error(`Expects all arguments of divide() to be finite numbers`);
+    }
+    return num1 / num2;
+  },
 }
